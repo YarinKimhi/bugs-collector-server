efd2aa17edde3a58df470473e208b8fb2ae4cea0
YarinKimhi <57398593+YarinKimhi@users.noreply.github.com>
2020-12-10 23:23:48 +0200

    progress

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -21,90 +21,39 @@ exports.registerController = (req,res) => {
         User.findOne({
             email
         }).exec((err,user)=>{
+            if(err){
+                return res.status(400).json({
+                    error: "Something went wrong try again"
+                })
+            }
             if(user){
                 return res.status(400).json({
                     error: "Email is taken"
                 })
             }else{
-                const token = jwt.sign(
-                    {
-                        name,
-                        email,
-                        password
-                    },
-                    process.env.JWT_ACCOUNT_ACTIVATION,
-                    {
-                        expiresIn:'15m'
-                    }
-                )
-                const emailData = {
-                    from: process.env.EMAIL_FROM,
-                    to: email,
-                    subject: "Accuont activation link",
-                    html:`
-                        <h1>Please Click the link to activate </h1>
-                        <p> ${process.env.URL}/user/activate/${token}</p>
-                        <hr/>
-                        <p> This email contain sensetive information</p>
-                        <p> ${process.env.URL}</p>
-                    `
-                }
-                sgMail
-                    .send(emailData)
-                    .then((sent)=>{
+                const user =new User({
+                    name,
+                    email,
+                    password
+                })
+                user.save((err,user)=>{
+                    if(err){
+                        return res.status(401).json({
+                            error:errorHandler(err)
+                        })
+                    }else{
                         return res.json({
-                            message: `email have been sent to ${email}`
+                            success: true,
+                            message: 'Signup success',
+                            user
                         })
-                    })
-                    .catch((err)=>{
-                        return res.status(400).json({
-                            error: errorHandler(err)
-                        })
-                    })
+                    }
+                }) 
             }
         })
-        
-
     }
 }
 
-exports.activationCotroller = (req,res)=>{
-    const {token} = req.body
-    if(token){
-        jwt.verify(token,process.env.JWT_ACCOUNT_ACTIVATION, 
-            (err,decode)=>{
-                if(err){
-                    return res.status(401).json({
-                        error:'Expired Token, signup again'
-                    })
-                }else{
-                    const {name,email,password} = jwt.decode(token)
-                    const user =new User({
-                        name,
-                        email,
-                        password
-                    })
-                    user.save((err,user)=>{
-                        if(err){
-                            return res.status(401).json({
-                                error:errorHandler(err)
-                            })
-                        }else{
-                            return res.json({
-                                success: true,
-                                message: 'Signup success',
-                                user
-                            })
-                        }
-                    })
-                }
-        })
-    }else{
-        return res.status(401).json({
-            message:'error , please try again'
-        })
-    }
-}
 
 exports.loginController = (req,res)=>{
     const {email,password} = req.body
@@ -151,3 +100,84 @@ exports.loginController = (req,res)=>{
         })
     }
 }
+
+
+
+/*
+const token = jwt.sign(
+                    {
+                        name,
+                        email,
+                        password
+                    },
+                    process.env.JWT_ACCOUNT_ACTIVATION,
+                    {
+                        expiresIn:'15m'
+                    }
+                )*/
+                /*const emailData = {
+                    from: process.env.EMAIL_FROM,
+                    to: email,
+                    subject: "Accuont activation link",
+                    html:`
+                        <h1>Please Click the link to activate </h1>
+                        <p> ${process.env.URL}/user/activate/${token}</p>
+                        <hr/>
+                        <p> This email contain sensetive information</p>
+                        <p> ${process.env.URL}</p>
+                    `
+                }
+                sgMail
+                    .send(emailData)
+                    .then((sent)=>{
+                        return res.json({
+                            message: `email have been sent to ${email}`
+                        })
+                    })
+                    .catch((err)=>{
+                        return res.status(400).json({
+                            error: errorHandler(err)
+                        })
+                    })
+                     */
+
+
+/*
+exports.activationCotroller = (req,res)=>{
+    const {token} = req.body
+    if(token){
+        jwt.verify(token,process.env.JWT_ACCOUNT_ACTIVATION, 
+            (err,decode)=>{
+                if(err){
+                    return res.status(401).json({
+                        error:'Expired Token, signup again'
+                    })
+                }else{
+                    const {name,email,password} = jwt.decode(token)
+                    const user =new User({
+                        name,
+                        email,
+                        password
+                    })
+                    user.save((err,user)=>{
+                        if(err){
+                            return res.status(401).json({
+                                error:errorHandler(err)
+                            })
+                        }else{
+                            return res.json({
+                                success: true,
+                                message: 'Signup success',
+                                user
+                            })
+                        }
+                    })
+                }
+        })
+    }else{
+        return res.status(401).json({
+            message:'error , please try again'
+        })
+    }
+}
+*/
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,18 +4,16 @@ const router = express.Router()
 const {
     validRegister,
     validLogin,
-    forgotPassword,
-    resetPasswordValidator
 } = require('../helpers/valid')
 
 const {
     registerController,
-    activationCotroller,
     loginController
 } = require('../controllers/auth.controller.js')
 
 router.post('/register',validRegister, registerController)
 router.post('/login',validLogin, loginController)
-router.post('/activation', activationCotroller)
 
 module.exports = router
+
+//router.post('/activation', activationCotroller)
